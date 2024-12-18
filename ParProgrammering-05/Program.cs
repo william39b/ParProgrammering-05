2ea7f9bd16d407a129a4596c460c8da0c9fea779
William B <william.buwembo21@gmail.com>
2024-12-18 11:02:10 +0100

    test

--- ParProgrammering-05/Program.cs
+++ ParProgrammering-05/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+1) Harry Pottah🥳l33t haxx0r
+Du skal starte med å lage en harrypotter character klasse med egenskaper som navn, house, inventory (ex wand eller pet)
+Få applikasjonen til å printe ut en introduksjon for karakteren, som sier noe om hva de heter, hvilket hus de er medlem av og hvilke items de har
+
+Karakteren skal ha mulighet til å gå inn i en Magibutikk, der kan de kjøpe et dyr:  ugle,rotte eller en katt. 
+De har også mulighet til å kjøpe en tryllestav: føniksstav, unikornstav eller trollstav. For å få til dette må du lage en egen klasse som er butikken, og presentere brukeren med en meny for hva personen skal kjøpe.
+
+Karakteren skal ha mulighet til å trylle - ta inn input fra brukeren, og dersom en skriver en riktig trylleformel skal det printes til skjermen at de har utført tryllingen
+Trylleformler: 
+vingardium leviosa (får en fjær til å fly)
+hokus pokus (fyrer av fyrverkerier)
+*/
+
+while (true)
+{
+    Console.WriteLine(
+        $"[1] Wizzard info\n" +
+        $"[2] Go to Magic store\n" +
+        $"[3] Cast a spell\n" +
+        $"[4] Create a new Wizzard\n" +
+        $"[5] Quit\n");
+    var input = Console.ReadLine();
+
+    switch (input)
+    {
+        case "1":
+            break;
+        case "2":
+            break;
+        case "3":
+            break;
+        case "4":
+            break;
+        case "5":
+            break;
+    }
+}
